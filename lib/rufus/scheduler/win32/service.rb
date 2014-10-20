78b080526a991392c0c3658618af4edc915c912d
Jurgens du Toit <jurgens@tutuka.com>
2014-10-20 12:44:36 +0200

    Rename the modules to not conflict with Rufus::Scheduler

--- lib/rufus/scheduler/win32/service.rb
+++ lib/rufus/scheduler/win32/service.rb
@@ -3,57 +3,52 @@ require 'logger'
 require 'win32/daemon'
 require 'rufus-scheduler'
 
-module Rufus
-  module Scheduler
-    # Containing module for the Service class
-    module Win32
-      include ::Win32
+module RufusSchedulerWin32
+  include ::Win32
 
-      # Service wrapper around rufus-scheduler
-      class Service
-        attr_writer :logger
+  # Service wrapper around rufus-scheduler
+  class Service
+    attr_writer :logger
 
-        def service_main
-          logger.info 'Running Main Loop'
-          while scheduler.up?
-            logger.debug 'Still Running'
-            sleep 10
-          end
-          logger.info 'Scheduler down. Exiting main loop'
-        end
-
-        def service_stop
-          logger.info 'Stopping scheduler service'
-
-          scheduler.shutdown(:wait)
-
-          logger.info 'Service stopped'
-        end
-
-        def service_pause
-          logger.info 'Pausing scheduler service'
-
-          scheduler.pause unless scheduler.paused?
-
-          logger.info 'Paused scheduler service'
-        end
-
-        def service_resume
-          logger.info 'Resuming scheduler service'
-
-          scheduler.resume if scheduler.paused?
-
-          logger.info 'Resumed scheduler service'
-        end
-
-        def scheduler
-          @scheduler ||= Rufus::Scheduler.new
-        end
-
-        def logger
-          @logger ||= Logger.new(STDOUT)
-        end
+    def service_main
+      logger.info 'Running Main Loop'
+      while scheduler.up?
+        logger.debug 'Still Running'
+        sleep 10
       end
+      logger.info 'Scheduler down. Exiting main loop'
+    end
+
+    def service_stop
+      logger.info 'Stopping scheduler service'
+
+      scheduler.shutdown(:wait)
+
+      logger.info 'Service stopped'
+    end
+
+    def service_pause
+      logger.info 'Pausing scheduler service'
+
+      scheduler.pause unless scheduler.paused?
+
+      logger.info 'Paused scheduler service'
+    end
+
+    def service_resume
+      logger.info 'Resuming scheduler service'
+
+      scheduler.resume if scheduler.paused?
+
+      logger.info 'Resumed scheduler service'
+    end
+
+    def scheduler
+      @scheduler ||= Rufus::Scheduler.new
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
     end
   end
 end
